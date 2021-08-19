acaaabb67e09b0632754d861c2f565face50de8a
lhj502819@163.com <lhj502819@163.com>
2021-08-19 17:28:22 +0800

    极客时间设计模式学习Project提交 1、扩展开放，修改关闭原则笔记提交

---
+++ Design-Patterns-GeekTimes/src/main/java/cn/onenine/design/patterns/principle/openclosed/OpenClosedPrinciple.java
@@ -0,0 +1,16 @@
+package cn.onenine.design.patterns.principle.openclosed;
+
+/**
+ * Description：对扩展开放，修改关闭原则：software entities(modules,classes,functions,etc)should be open for extension but closed for modification
+ *                      软件实体（模块、类、方法等）应该“对扩展开放，对修改关闭”
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2021/8/19
+ */
+public class OpenClosedPrinciple {
+    /**
+     * 添加一个新的功能应该是在已有代码基础上扩展代码（新增模块、类、方法等），而非修改已有代码（修改模块、类、方法等）
+     */
+
+}
---
+++ Design-Patterns-GeekTimes/src/main/java/cn/onenine/design/patterns/principle/openclosed/old/Alert.java
@@ -0,0 +1,12 @@
+package cn.onenine.design.patterns.principle.openclosed.old;
+
+/**
+ * Description：接口监控告警相关
+ *
+ * @author li.hongjian
+ * @email lhj502819@163.com
+ * @since 2021/8/19
+ */
+public class Alert {
+
+}
